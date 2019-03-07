f5081fd28391f993c37ca549007a500fea9895a8
serhiy2000 <3774217@gmail.com>
2019-03-08 00:02:13 +0200

    first trial 8.03.2019

---
+++ src/lesson12HashSetTreeSetLinkedSet/Car.java
@@ -0,0 +1,76 @@
+package lesson12HashSetTreeSetLinkedSet;
+
+import java.util.*;
+
+public class Car {
+
+    private String model;
+    private int age;
+    private String color;
+
+    public Car(String model, int age, String color) {
+        this.model = model;
+        this.age = age;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", age=" + age +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+    Car car1 = new Car ("Opel", 2, "red");
+    Car car2 = new Car ("Toyota", 3, "black");
+    Car car3 = new Car ("Mazda", 3, "green");
+    Car car4 = new Car ("Nissan", 23, "white");
+
+
+    ArrayList<Car> single = new ArrayList();
+    single.add(car1);
+    System.out.println(single.get(0).model);
+
+
+    LinkedHashSet<Car> triple = new LinkedHashSet<>();
+    triple.add(car1);
+    triple.add(car2);
+    triple.add(car3);
+
+    System.out.println(triple);
+
+
+
+    }
+
+// в один момент в одному гаражі може бути одна машина, в іншому може бути три машини
+
+}

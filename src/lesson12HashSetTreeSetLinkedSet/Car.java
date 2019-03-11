b98de70ba5e7e0807ef613074a7ef1c7f004394e
serhiy2000 <3774217@gmail.com>
2019-03-11 22:33:54 +0200

    пробую робити дом завдання

--- src/lesson12HashSetTreeSetLinkedSet/Car.java
+++ src/lesson12HashSetTreeSetLinkedSet/Car.java
@@ -1,14 +1,21 @@
 package lesson12HashSetTreeSetLinkedSet;
 
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class Car {
 
-    private String model;
-    private int age;
-    private String color;
+    public String model;
+    public int age;
+    public String color;
 
     public Car(String model, int age, String color) {
+        this.model = model;
+        this.age = age;
+        this.color = color;
+    }
+
+    public Car(String model, int age, int price) {
         this.model = model;
         this.age = age;
         this.color = color;
@@ -54,21 +61,40 @@ public class Car {
 
 
     ArrayList<Car> single = new ArrayList();
-    single.add(car1);
+    single.add(car2);
     System.out.println(single.get(0).model);
 
 
     LinkedHashSet<Car> triple = new LinkedHashSet<>();  // чудова заготовка на кошик у нашій аппці
 
-    triple.add(car1);
-    triple.add(car1);
-    triple.add(car3);
+        triple.add(car1);
+        System.out.println(triple.add(car1));
+        triple.add(car3);
 
+
+    Iterator <Car> iterator = triple.iterator();
+    while (iterator.hasNext()){
+        Car aCar = iterator.next();
+        System.out.println("Message from Iterator"+aCar);
+        System.out.println("iterator.hasNext" + iterator.hasNext());
+    }
 
     System.out.println(triple);     // тут використовує одверайднутий метод toString
 
     for (Car model : triple)
         System.out.println(model.model);   // це кльово працює.
+
+        ArrayList <Car> basket = new ArrayList<>();
+
+        if (triple.add(car4) == true){
+            System.out.println("Added element to the triple carwash"+car4);
+            int q=1;
+
+            Car busket = new Car(car4.getModel(), q , 125);
+            System.out.println("basket"+busket);
+        };
+
+
 
 
         // to upload to GITHUB - it is necessary to Commit and Push in two steps.
